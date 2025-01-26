7143aff397fc8b9e47bf46097b802a9b3d3339b5
FabioBritto <fabio.tritono@gmail.com>
2025-01-26 00:31:07 -0300

    Adicionadas funcionalidades teste ao MainController

--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -1,5 +1,42 @@
 package gui;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemVendedor;
+	
+	@FXML
+	private MenuItem menuItemDepartamento;
+	
+	@FXML
+	private MenuItem menuItemSobre;
+	
+	@FXML
+	public void onMenuItemVendedorAction() {
+		System.out.println(".");
+	}
+	
+	@FXML
+	public void onMenuItemDepartamentoAction() {
+		System.out.println(".");
+	}
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		System.out.println(".");
+	}
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
