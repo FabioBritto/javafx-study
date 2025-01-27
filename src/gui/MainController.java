9f44facd841db653ea18b61cef412d6f2b17c725
FabioBritto <fabio.tritono@gmail.com>
2025-01-27 19:21:11 -0300

    Criada interface About

--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -1,41 +1,79 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainController implements Initializable {
 
 	@FXML
-	private MenuItem menuItemVendedor;
+	private MenuItem menuItemSeller;
 	
 	@FXML
-	private MenuItem menuItemDepartamento;
+	private MenuItem menuItemDepartment;
 	
 	@FXML
-	private MenuItem menuItemSobre;
+	private MenuItem menuItemAbout;
 	
 	@FXML
-	public void onMenuItemVendedorAction() {
+	public void onMenuItemSellerAction() {
 		System.out.println(".");
 	}
 	
 	@FXML
-	public void onMenuItemDepartamentoAction() {
+	public void onMenuItemDepartmentAction() {
 		System.out.println(".");
 	}
 	
 	@FXML
-	public void onMenuItemSobreAction() {
-		System.out.println(".");
+	public void onMenuItemAboutAction() {
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	/*
+	 * Uso o Synchronized para garantir que todo o processamento do método ocorra sem ser interrompido.
+	 * Como a aplicação é MultiThread, isso é necessário
+	 */
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			//getRoot() pega o primeiro elemento da View
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			//Aqui, eu pego o children na posição ZERO
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			/*
+			 * Agora, eu adiciono uma COLEÇÃO, que é justamente os filhos do newVBox instanciado
+			 * no início do método. Esse newVBox na verdade foi setado por seu abosulteName
+			 */
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Error Loading View", e.getMessage(), AlertType.ERROR);
+		}
 		
 	}
 
