445d15f87c9aea0fed2c222437529efb56eb3646
FabioBritto <fabio.tritono@gmail.com>
2025-02-02 14:09:14 -0300

    Criado DepartmentForm (dialog)

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,16 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+
+	/*
+	 * Função que recupera o Stage a partir do EVENTO (o clicar de um botão, por exemplo)
+	 */
+	public static Stage currentStage(ActionEvent event) {
+		Stage currentStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		return currentStage;
+	}
+}
